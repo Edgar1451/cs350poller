b8553b55f980213108ad15f6a481067d5eea926d
Edgar Larios <edgarlarios1451@gmail.com>
2018-03-29 15:14:04 -0600

    Add homepage

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.views.generic import TemplateView
 from django.shortcuts import render
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
 # Create your views here.
--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-
+from django.views.generic import TemplateView
 from .models import Choice, Question
 
 
